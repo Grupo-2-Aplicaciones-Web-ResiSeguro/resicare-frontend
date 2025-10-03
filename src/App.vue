bc0e944f5c7ba1ecaa5c00a759b93b6f073aa3e8
Héctor Ríos <JHector317031@gmail.com>
2025-10-03 15:38:17 -0500

    Initial commit: setup Vue project with Vite, PrimeVue, i18n and general components

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <main-layout />
+</template>
+
+<script setup>
+import MainLayout from './shared-kernel/presentation/layouts/default/main-layout.component.vue'
+</script>
+
+<style scoped></style>
